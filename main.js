c3bcb38619c08c8615fb5b7bcfbe33a7813bcaaf
Your Name <darkcreationsl98@gmail.com>
2025-01-21 20:36:13 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,4 @@
+let nameElement=document.getElementById('name');
+let addressElement=document.getElementById('address');
+let salaryElement=document.getElementById('salary');
+let dobElement=document.getElementById('dob');
